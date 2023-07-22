f951fd5d43231a2ae8c4088c8733b16ece25490f
m4ggiee <121451790+m4ggiee@users.noreply.github.com>
2023-07-22 12:28:02 +0300

    first commit

---
+++ homework2.1/JS.js
@@ -0,0 +1,39 @@
+//1
+let name = prompt(" What's your name?");
+let greet = alert (`Hello, ${name}! How are you? `);
+
+//2
+let a = 55;
+let b = 25;
+
+console.log(a + b , a-b, a/b, a*b);
+
+//3
+let comparison1 = prompt(`Enter the first value you want to compare:`);
+let comparison2 = prompt(`Enter the second value you want to compare:`);
+
+console.log(comparison1 === comparison2);
+
+//4
+let c = 12;
+
+console.log((a+b+c)/3);
+
+//5
+console.log(23156 % 10);
+console.log(2315 % 10);
+console.log(231 % 10)
+console.log(23 % 10);
+console.log(2 % 10);
+
+// var num = 23156 ;
+// var digits = [];
+// while (num > 0) {
+//     var remainder = num % 10; 
+//     digits.push(remainder); 
+//     num = Math.floor(num / 10); 
+// }
+
+// var output = digits.join(" "); 
+// console.log(output); 
+
---
+++ homework2.2/js.js
@@ -0,0 +1,3 @@
+let hour = prompt(`Enter the number of hours to get seconds:`);
+let seconds = hour*3600;
+let results = alert(`Your result: ${seconds}s `);
